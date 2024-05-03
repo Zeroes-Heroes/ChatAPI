8ec8af2440e5a10445de8f88ce2755101d062ced
Naum Vitanov <vitanovnaum@gmail.com>
2024-05-04 00:32:51 +0300

    Commented the add friend logic.

--- ChatAPI.Application/UseCases/Implementations/FriendshipService.cs
+++ ChatAPI.Application/UseCases/Implementations/FriendshipService.cs
@@ -10,21 +10,21 @@ namespace ChatAPI.Application.UseCases.Implementations
 	{
 		public async Task<Result> AddFriend(int senderUserId, string targetEmail)
 		{
-			User? targetUser = await userRepo.GetUserByEmailAsNoTracking(targetEmail);
+			//User? targetUser = await userRepo.GetUserByEmailAsNoTracking(targetEmail);
 
-			if (targetUser is null)
-				return Result.Failure("User with that email is not found.");
+			//if (targetUser is null)
+			//	return Result.Failure("User with that email is not found.");
 
-			if (senderUserId == targetUser.Id)
-				return Result.Failure("You cannot add yourself as a friend.");
+			//if (senderUserId == targetUser.Id)
+			//	return Result.Failure("You cannot add yourself as a friend.");
 
-			Friendship? friendship = await friendRepo.GetFriendshipByUserIds([senderUserId, targetUser.Id]);
+			//Friendship? friendship = await friendRepo.GetFriendshipByUserIds([senderUserId, targetUser.Id]);
 
-			if (friendship is not null)
-				return Result.Failure("You are already friends with that user.");
+			//if (friendship is not null)
+			//	return Result.Failure("You are already friends with that user.");
 
-			await friendRepo.AddFriendship(senderUserId, targetUser.Id);
-			return Result.Success();
+			//await friendRepo.AddFriendship(senderUserId, targetUser.Id);
+			//return Result.Success();
 		}
 
 		public Task<IEnumerable<FriendDTO>> GetUserFriendships(int userId, FriendshipStatus? status = null, bool? isInitiator = null) =>
