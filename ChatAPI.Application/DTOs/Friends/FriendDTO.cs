140264b0f25262b1919360398a7b2930d4bbc2ae
Valentino Gechev <valentinogechev@gmail.com>
2024-05-20 19:14:09 +0300

    Change to userId instead of friendUserId

--- ChatAPI.Application/DTOs/Friends/FriendDTO.cs
+++ ChatAPI.Application/DTOs/Friends/FriendDTO.cs
@@ -2,5 +2,5 @@
 
 namespace ChatAPI.Application.DTOs.Friends
 {
-	public record FriendDTO(int FriendUserId, string Email, string Phone, FriendshipStatus FriendshipStatus, bool IsInitiator);
+	public record FriendDTO(int UserId, string Email, string Phone, FriendshipStatus FriendshipStatus, bool IsInitiator);
 }
