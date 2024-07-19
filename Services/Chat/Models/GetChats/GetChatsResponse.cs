d3e97502ce8069608abd90e6a37e184890214b19
Naum Vitanov <vitanovnaum@gmail.com>
2024-07-19 22:22:40 +0300

    Changed the response model of the get chats request to return chatId instead of just id

--- Services/Chat/Models/GetChats/GetChatsResponse.cs
+++ Services/Chat/Models/GetChats/GetChatsResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace Services.Chat.Models.GetChats;
 
-public record GetChatsResponse(int Id, string Name, int[] UserIds);
+public record GetChatsResponse(int ChatId, string Name, int[] UserIds);
