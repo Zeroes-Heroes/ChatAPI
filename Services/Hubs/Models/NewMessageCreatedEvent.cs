0e298ac850bf4bef4d15d7306ec5b84527b52f13
Naum Vitanov <vitanovnaum@gmail.com>
2024-07-20 13:02:36 +0300

    New dateTime format fort the events

--- Services/Hubs/Models/NewMessageCreatedEvent.cs
+++ Services/Hubs/Models/NewMessageCreatedEvent.cs
@@ -1,4 +1,8 @@
 ﻿namespace Services.Hubs.Models
 {
-	public record NewMessageCreatedEvent(int Id, int ChatId, int TempId, DateTime CreatedAt);
+	public class NewMessageCreatedEvent(int Id, int ChatId, int TempId, string CreatedAt)
+	{
+		public NewMessageCreatedEvent(int Id, int ChatId, int TempId, DateTime CreatedAt)
+			: this(Id, ChatId, TempId, CreatedAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'")) { }
+	}
 }
--- Services/Hubs/Models/NewMessageEvent.cs
+++ Services/Hubs/Models/NewMessageEvent.cs
@@ -1,4 +1,8 @@
 ﻿namespace Services.Hubs.Models
 {
-	public record NewMessageEvent(int Id, int SenderUserId, int ChatId, string Content, DateTime CreatedAt);
+	public class NewMessageEvent(int Id, int SenderUserId, int ChatId, string Content, string CreatedAt)
+	{
+		public NewMessageEvent(int Id, int SenderUserId, int ChatId, string Content, DateTime CreatedAt)
+			: this(Id, SenderUserId, ChatId, Content, CreatedAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'")) { }
+	}
 }
