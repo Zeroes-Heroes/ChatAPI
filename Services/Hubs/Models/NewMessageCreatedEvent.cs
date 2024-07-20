b2fe2ae8a2eb940d8420deae5dc5efc8b1fc3cf0
Naum Vitanov <vitanovnaum@gmail.com>
2024-07-20 17:15:23 +0300

    Added empty constructor for the signalR to map

--- Services/Hubs/Models/NewMessageCreatedEvent.cs
+++ Services/Hubs/Models/NewMessageCreatedEvent.cs
@@ -1,8 +1,23 @@
 ﻿namespace Services.Hubs.Models
 {
-	public class NewMessageCreatedEvent(int Id, int ChatId, int TempId, string CreatedAt)
+	public class NewMessageCreatedEvent
 	{
-		public NewMessageCreatedEvent(int Id, int ChatId, int TempId, DateTime CreatedAt)
-			: this(Id, ChatId, TempId, CreatedAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'")) { }
+		public NewMessageCreatedEvent(int id, int chatId, int tempId, DateTime createdAt)
+		{
+			Id = id;
+			ChatId = chatId;
+			TempId = tempId;
+			CreatedAt = createdAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'");
+		}
+
+		public NewMessageCreatedEvent()
+		{
+
+		}
+
+		public int Id { get; set; }
+		public int ChatId { get; set; }
+		public int TempId { get; set; }
+		public string CreatedAt { get; set; }
 	}
 }
--- Services/Hubs/Models/NewMessageEvent.cs
+++ Services/Hubs/Models/NewMessageEvent.cs
@@ -1,8 +1,26 @@
 ﻿namespace Services.Hubs.Models
 {
-	public class NewMessageEvent(int Id, int SenderUserId, int ChatId, string Content, string CreatedAt)
+	public class NewMessageEvent
 	{
-		public NewMessageEvent(int Id, int SenderUserId, int ChatId, string Content, DateTime CreatedAt)
-			: this(Id, SenderUserId, ChatId, Content, CreatedAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'")) { }
+		public NewMessageEvent(int id, int senderUserId, int chatId, string content, DateTime createdAt)
+		{
+			Id = id;
+			SenderUserId = senderUserId;
+			ChatId = chatId;
+			Content = content;
+			CreatedAt = createdAt.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'");
+		}
+
+		public NewMessageEvent()
+		{
+
+		}
+
+		public int Id { get; set; }
+		public int SenderUserId { get; set; }
+		public int ChatId { get; set; }
+		public string Content { get; set; }
+		public string CreatedAt { get; set; }
+
 	}
 }
