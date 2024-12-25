a1e0aafdc6996a46d1b173d5258e95bfb3f53285
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-12-25 17:29:29 +0000

    feat: Enhance UserRepository with GetIdByDeviceId method

--- Services/Repositories/User/Repository/UserRepository.cs
+++ Services/Repositories/User/Repository/UserRepository.cs
@@ -77,4 +77,7 @@ public class UserRepository(AppDbContext dbContext) : IUserRepository
 	/// <returns>Array of user entities.</returns>
 	public Task<UserEntity[]> GetUsers(int[] userIds) =>
 		dbContext.Users.Where(u => userIds.Contains(u.Id)).ToArrayAsync();
+
+	public Task<UserDeviceEntity?> GetIdByDeviceId(string deviceId) =>
+		dbContext.UserDevices.FirstOrDefaultAsync(d => d.DeviceId == deviceId);
 }
