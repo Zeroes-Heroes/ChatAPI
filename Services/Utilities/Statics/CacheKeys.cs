ae12c0b84b8d29b132f9000e9ad1d397d2bcf66b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2025-01-17 18:13:32 +0000

    style: rename file with more correct name

--- Services/Utilities/Statics/CacheKeys.cs
+++ Services/Utilities/Statics/CacheKeys.cs
@@ -6,5 +6,5 @@ public static class CacheKeys
 	public const string RefreshToken = "{0}_refresh_token";
 	public const string ChatEntered = "{0}_chat_entered_{1}";
 	public const string ConnectionEstablished = "{0}_connection_established";
-	public const string ApplePushNotificationToken = "ApplePushNotificationToken"; // Why is used {0}?
+	public const string ApplePushNotificationToken = "ApplePushNotificationToken";
 }
